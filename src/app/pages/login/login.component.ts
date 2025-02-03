be13b81828610140f40515e71b3ccf2e25a128fa
imane-bouabidi <imaneyoucode@gmail.com>
2025-02-03 17:29:16 +0100

    login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import {NgOptimizedImage} from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-login',
   imports: [
+    CommonModule,
     NgOptimizedImage
   ],
   templateUrl: './login.component.html',
