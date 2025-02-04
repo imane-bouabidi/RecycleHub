c4a7403daf7ba46301f2a5684b97085a66b3b03d
imane-bouabidi <imaneyoucode@gmail.com>
2025-02-04 11:12:41 +0100

    some installation and ngrx

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {FormControl} from '@angular/forms';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-signup',
-  imports: [RouterLink],
+  imports: [RouterLink,CommonModule,
+    NgOptimizedImage],
   templateUrl: './signup.component.html',
   standalone: true,
   styleUrl: './signup.component.scss'
